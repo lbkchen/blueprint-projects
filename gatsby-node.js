e2804356c2be9dec6b7d4688149199abe6710969
Ken Chen <lbkchen@gmail.com>
2018-07-02 22:40:25 -0700

    Fixes gatsby-node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ const { createFilePath } = require('gatsby-source-filesystem')
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
 
+  // Get all markdown pages and generate website pages from markdown and templates
   return graphql(`
     {
       allMarkdownRemark(limit: 1000) {
@@ -27,43 +28,18 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       result.errors.forEach(e => console.error(e.toString()))
       return Promise.reject(result.errors)
     }
-    const posts = result.data.allMarkdownRemark.edges
 
-    posts.forEach(edge => {
-      const id = edge.node.id
+    const pages = result.data.allMarkdownRemark.edges
+    pages.forEach(page => {
+      const id = page.node.id
       createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
+        path: page.node.fields.slug,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
-        // additional data can be passed via context
         context: {
           id,
-        },
-      })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
+          slug: page.node.fields.slug,
         },
       })
     })
