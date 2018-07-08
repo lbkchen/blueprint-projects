5c0cea7cc1e3d2bb08356cf8dc4ede8376e0373a
Ken Chen <lbkchen@gmail.com>
2018-07-08 16:23:17 -0700

    Fixes nav.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
 
 import logo from '../img/logo.png'
 import './all.scss'
@@ -8,13 +9,15 @@ import './all.scss'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Blueprint Projects" />
-    <section className="section">
-      <div className="title-container">
-        <span className="title-projects is-size-3">projects</span>
-        <img className="title-image" src={logo} alt="logo" />
-        <span className="title-blueprint is-size-3">blueprint</span>
-      </div>
-    </section>
+    <nav className="nav-container">
+      <Link to="/">
+        <div className="title-container">
+          <span className="title-projects is-size-3">projects</span>
+          <img className="title-image" src={logo} alt="logo" />
+          <span className="title-blueprint is-size-3">blueprint</span>
+        </div>
+      </Link>
+    </nav>
     <div>{children()}</div>
   </div>
 )
